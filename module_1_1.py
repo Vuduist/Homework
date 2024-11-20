a08491dfa2603a2389c562c993de2bd7d41b662f
Vuduist <66877692+Vuduist@users.noreply.github.com>
2024-11-20 22:43:48 +0300

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,5 @@
+Number_of_tasks = 12
+Number_of_hours = 1.5
+Course_name = ('Python')
+Time_per_task = Number_of_hours/Number_of_tasks
+print('Курс: ', Course_name, ' Всего задач: ', Number_of_tasks, ' Затрачено часов: ', Number_of_hours, ' Среднее время выполнения: ', Time_per_task, 'часа.')
