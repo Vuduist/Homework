70ca7b70e8e13c69e3189f20b37d9add49093a81
Vuduist <66877692+Vuduist@users.noreply.github.com>
2024-11-26 20:01:48 +0300

    Домашка

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,8 @@
-list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-len = len(list)
+list1 = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+len1 = len(list1)
 i = 0
-while i <= len:
-    if list[i] > 0:
-        print(list[i])
+while i <= len1:
+    if list1[i] > 0:
+        print(list1[i])
         i = i+1
-    if list[i] < 0: break
+    if list1[i] < 0: break
